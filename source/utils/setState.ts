472b12faca008bf55daeaec61b203a05831e380b
FowluhhDev <trfowluhh@gmail.com>
2025-07-08 08:12:23 -0400

    jsdoc for setState and npm

--- source/utils/setState.ts
+++ source/utils/setState.ts
@@ -1,5 +1,12 @@
 import { Process, State } from "../types/process";
 
+/**
+ * Sets the state of a process
+ * @param {any} process The target process
+ * @param {any} state The state to set
+ * @returns {any}
+ * @since v0.0.1
+ */
 function setState(process: Process | undefined, state: State) {
     if (process) process.state = state;
 }
